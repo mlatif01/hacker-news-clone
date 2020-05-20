ab2cb96a9c1138a84d78d46bb0449a0fef465218
Milad Latif <miladlatif89@gmail.com>
2020-05-20 14:23:53 +0100

    completed hacker news clone

--- app/index.js
+++ app/index.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles.css';
+import './index.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from './contexts/theme';
+import Loading from './components/Loading';
+import Nav from './components/Nav';
 
-import News from './components/News';
+const Posts = React.lazy(() => import('./components/Posts'));
+const Post = React.lazy(() => import('./components/Post'));
+const User = React.lazy(() => import('./components/User'));
 
-class App extends React.Component {
+export default class App extends React.Component {
+  state = {
+    theme: 'light',
+    toggleTheme: () => {
+      this.setState(({ theme }) => ({
+        theme: theme === 'light' ? 'dark' : 'light',
+      }));
+    },
+  };
   render() {
     return (
-      <div className='container'>
-        <News />
-      </div>
+      <Router>
+        <ThemeProvider value={this.state}>
+          <div className={this.state.theme}>
+            <div className='container'>
+              <Nav />
+
+              <React.Suspense fallback={<Loading />}>
+                <Switch>
+                  <Route exact path='/' render={() => <Posts type='top' />} />
+                  <Route path='/new' render={() => <Posts type='new' />} />
+                  <Route path='/post' component={Post} />
+                  <Route path='/user' component={User} />
+                  <Route render={() => <h1>404</h1>} />
+                </Switch>
+              </React.Suspense>
+            </div>
+          </div>
+        </ThemeProvider>
+      </Router>
     );
   }
 }
